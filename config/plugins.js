158ec18558a06488f0bc26a184b4a436f270d890
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-07 17:14:19 -0300

    Criando usuários, esqueci minha senha e e-mail via SMTP (Gmail)

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,32 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'cloudinary',
+    provider: "cloudinary",
     providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
     },
     actionOptions: {
       upload: {},
       delete: {},
+    },
+  },
+
+  email: {
+    provider: "smtp",
+    providerOptions: {
+      host: "smtp.gmail.com", //SMTP Host
+      port: 465, //SMTP Port
+      secure: true,
+      username: "__SEU_EMAIL@gmail.com",
+      password: "__SUA_SENHA_DE_APP__",
+      rejectUnauthorized: true,
+      requireTLS: true,
+      connectionTimeout: 1,
+    },
+    settings: {
+      from: "__SEU_EMAIL@gmail.com",
+      replyTo: "__SEU_EMAIL@gmail.com",
     },
   },
 });
