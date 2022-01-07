a5326fcecf6e86d57579860fd1da446752a75f1a
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-07 00:53:57 -0300

    Update e Delete

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -9,10 +9,24 @@ module.exports = {
 
   async create(ctx) {
     const { id } = ctx.state.user;
-    const { title, content } = ctx.request.body;
-    const post = { title, content, user: id };
 
-    const entity = await strapi.services.post.create(post);
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.create(
+        {
+          ...data,
+          user: id,
+        },
+        { files }
+      );
+    } else {
+      entity = await strapi.services.post.create({
+        ...ctx.request.body,
+        user: id,
+      });
+    }
+
     return sanitizeEntity(entity, { model: strapi.models.post });
   },
 
@@ -30,5 +44,66 @@ module.exports = {
     return entities.map((entity) =>
       sanitizeEntity(entity, { model: strapi.models.post })
     );
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const entity = await strapi.services.post.findOne({
+      id,
+      user: ctx.state.user.id,
+    });
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+
+  async count(ctx) {
+    const query = { ...ctx.query, user: ctx.state.user.id };
+
+    if (ctx.query._q) {
+      return await strapi.services.post.countSearch(query);
+    }
+
+    return await strapi.services.post.count(query);
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const post = await strapi.services.post.find({
+      id,
+      user: ctx.state.user.id,
+    });
+
+    if (!post || !post.length) {
+      return ctx.unauthorized("You cannot update this post.");
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.post.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const post = await strapi.services.post.find({
+      id,
+      user: ctx.state.user.id,
+    });
+
+    if (!post || !post.length) {
+      return ctx.unauthorized("You cannot delete this post.");
+    }
+
+    const entity = await strapi.services.post.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.post });
   },
 };
